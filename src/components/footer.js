6fc4dc06df9d9c14fdf91a78b01f97adbe36a27e
Hardik Ranjan <hardikranjan@Hardiks-MacBook-Pro.local>
2023-07-08 13:59:49 +0530

    Updated styles

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,135 +1,48 @@
-import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { FormattedIcon } from '@components/icons';
-import { socialMedia } from '@config';
-import styled from 'styled-components';
-import { theme, mixins, media } from '@styles';
-const { colors, fontSizes, fonts } = theme;
+import React, { Component } from "react";
 
-const StyledContainer = styled.footer`
-  ${mixins.flexCenter};
-  flex-direction: column;
-  padding: 15px;
-  text-align: center;
-  height: auto;
-  min-height: 70px;
-`;
-const StyledSocial = styled.div`
-  color: ${colors.lightSlate};
-  width: 100%;
-  max-width: 270px;
-  margin: 0 auto 10px;
-  display: none;
-  ${media.tablet`display: block;`};
-`;
-const StyledSocialList = styled.ul`
-  ${mixins.flexBetween};
-  padding: 0;
-  margin: 0;
-  list-style: none;
-`;
-const StyledSocialLink = styled.a`
-  padding: 10px;
-  svg {
-    width: 20px;
-    height: 20px;
-  }
-`;
-const StyledMetadata = styled.div`
-  font-family: ${fonts.SFMono};
-  font-size: ${fontSizes.xs};
-  line-height: 1;
-`;
-const StyledGitHubLink = styled.a`
-  color: ${colors.lightSlate};
-  padding: 10px;
-`;
-const StyledGitHubInfo = styled.div`
-  margin-top: 10px;
-
-  & > span {
-    display: inline-flex;
-    align-items: center;
-    margin: 0 7px;
-  }
-  svg {
-    display: inline-block;
-    height: 15px;
-    width: auto;
-    margin-right: 5px;
-  }
-`;
-
-const Footer = () => {
-  const [githubInfo, setGitHubInfo] = useState({
-    stars: null,
-    forks: null,
-  });
-
-  useEffect(() => {
-    if (process.env.NODE_ENV !== 'production') {
-      return;
-    }
-    fetch('https://api.github.com/repos/bchiang7/v4')
-      .then(response => response.json())
-      .then(json => {
-        const { stargazers_count, forks_count } = json;
-        setGitHubInfo({
-          stars: stargazers_count,
-          forks: forks_count,
-        });
-      })
-      .catch(e => console.error(e));
-  }, []);
-
-  return (
-    <StyledContainer>
-      <StyledSocial>
-        <StyledSocialList>
-          {socialMedia &&
-            socialMedia.map(({ name, url }, i) => (
-              <li key={i}>
-                <StyledSocialLink
-                  href={url}
-                  target="_blank"
-                  rel="nofollow noopener noreferrer"
-                  aria-label={name}>
-                  <FormattedIcon name={name} />
-                </StyledSocialLink>
-              </li>
-            ))}
-        </StyledSocialList>
-      </StyledSocial>
-      <StyledMetadata tabindex="-1">
-        <StyledGitHubLink
-          href="https://github.com/bchiang7/v4"
-          target="_blank"
-          rel="nofollow noopener noreferrer">
-          <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Chandrika Deb
+class Footer extends Component {
+  render() {
+    return (
+      <footer id="contact">
+        <div className="container">
+          <blockquote className="text-center">
+            <p className="quotation mb-5">Ever tried. Ever failed. No matter. Try again. Fail again. Fail better.</p>
+          </blockquote>
+          <div className="social-links text-center pb-5">
+            <a className="mx-3" href="https://twitter.com/AsheeshKumar34" aria-label="twitter" target="_blank" rel="noopener noreferrer">
+              <span className="icon-container p-2 d-inline-block rounded-circle">
+                <i className="fa fa-twitter fa-lg"></i>
+              </span>
+            </a>
+            <a className="mx-3" href="https://www.facebook.com/asheesh.kumar.9465" aria-label="facebook" target="_blank" rel="noopener noreferrer">
+              <span className="icon-container p-2 d-inline-block rounded-circle">
+                <i className="fa fa-facebook fa-lg"></i>
+              </span>
+            </a>
+            <a className="mx-3" href="https://www.linkedin.com/in/theasheeshkumar/" aria-label="linkedin" target="_blank" rel="noopener noreferrer">
+              <span className="icon-container p-2 d-inline-block rounded-circle">
+                <i className="fa fa-linkedin fa-lg"></i>
+              </span>
+            </a>
+            <a className="mx-3" href="https://www.instagram.com/asheeshkumar34/" aria-label="instagram" target="_blank" rel="noopener noreferrer">
+              <span className="icon-container p-2 d-inline-block rounded-circle">
+                <i className="fa fa-instagram fa-lg"></i>
+              </span>
+            </a>
+            <a className="mx-3 d-inline-block mt-4 mt-md-0" href="mailto: asheesh.kumar3496@gmail.com" aria-label="email" target="_blank" rel="noopener noreferrer">
+              <span className="icon-container p-2 d-inline-block rounded-circle">
+                <i className="fa fa-envelope fa-lg"></i>
+              </span>
+            </a>
           </div>
-
-          {githubInfo.stars && githubInfo.forks && (
-            <StyledGitHubInfo>
-              <span>
-                <FormattedIcon name="Star" />
-                <span>{githubInfo.stars.toLocaleString()}</span>
-              </span>
-              <span>
-                <FormattedIcon name="Fork" />
-                <span>{githubInfo.forks.toLocaleString()}</span>
-              </span>
-            </StyledGitHubInfo>
-          )}
-        </StyledGitHubLink>
-      </StyledMetadata>
-    </StyledContainer>
-  );
-};
-
-Footer.propTypes = {
-  githubInfo: PropTypes.object,
-};
+          <div className="copyright text-center">
+            <span className="d-block">Handcrafted by me over a cup of tea</span>
+            <span className="d-inline-block">Copyright &copy; 2021</span>
+          </div>
+        </div>
+      </footer>
+    );
+  }
+}
 
 export default Footer;
